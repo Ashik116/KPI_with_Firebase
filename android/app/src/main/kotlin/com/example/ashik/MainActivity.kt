8e1c9671533883fe0a89a2ab77495c1fc0fb3f23
Ashik116 <ashikrahaman542@gmail.com>
2022-02-27 15:00:40 +0600

    27/02/2022

---
+++ android/app/src/main/kotlin/com/example/ashik/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ashik
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/newpage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Ashik extends StatelessWidget {
+  const Ashik({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Done"),
+      ),
+    );
+  }
+}

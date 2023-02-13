fd36ae5d1ded4618303032c0694d75b3b545a4c2
MD. Ashikur Rahman <99240744+Ashik116@users.noreply.github.com>
2023-02-13 18:03:25 +0600

    Ashik-11/12-2022

--- android/app/src/main/kotlin/com/example/ashik/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ashik
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/example/kpi_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kpi.regnumold.v
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
